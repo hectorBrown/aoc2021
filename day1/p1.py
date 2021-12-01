e077718f8dd4db0fb2f5114a87bd6333acfdef9e
Hector Brown <hectorjbrown@protonmail.com>
2021-12-01 14:27:40 +0000

    day1

---
+++ day1/p1.py
@@ -0,0 +1,9 @@
+PATH = "day1/data.txt"
+input = [int(x[:-1]) for x in open(PATH).readlines()]
+
+count = 0
+for i in range(1, len(input)):
+    if input[i - 1] < input[i]:
+        count += 1
+
+print(count)
---
+++ day1/p2.py
@@ -0,0 +1,9 @@
+PATH = "day1/data.txt"
+input = [int(x[:-1]) for x in open(PATH).readlines()]
+
+count = 0
+for i in range(1, len(input) - 2):
+    if sum(input[i-1:i+2]) < sum(input[i:i+3]):
+        count += 1
+
+print(count)
