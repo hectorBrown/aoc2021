e52848a348ac1c04796f23ad5e7d137746b8e707
Hector Brown <hectorjbrown@protonmail.com>
2021-12-07 12:17:01 +0000

    day 7

---
+++ day7/p1.py
@@ -0,0 +1,16 @@
+PATH = "day7/data.txt"
+
+def cost(crabs, target):
+    return sum(map(lambda x : abs(x - target), crabs))
+
+crabs = [int(x) for x in open(PATH).readline().split(",")]
+
+target = 0; currcost = cost(crabs, 0)
+for i in range(min(crabs), max(crabs)):
+    if cost(crabs, i) < currcost:
+        currcost = cost(crabs, i)
+        target = i
+
+print(cost(crabs, target))
+
+
